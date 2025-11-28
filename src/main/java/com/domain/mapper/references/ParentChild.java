c23d554f5828984678d805f4bda39da344b5eaee
sougata <sougatadas@cdac.in>
2025-11-28 15:55:47 +0530

    added level childName field in the ParentChild

--- src/main/java/com/domain/mapper/references/ParentChild.java
+++ src/main/java/com/domain/mapper/references/ParentChild.java
@@ -3,23 +3,33 @@ package com.domain.mapper.references;
 public class ParentChild<P, C> {
     private final P parent;
     private final C child;
-    private int level;
+    private final int level;
+    private final String childName;
 
-    public ParentChild(P parent, C child) {
-        this(parent, child, 10);
+    public ParentChild(P parent, C child, String childName) {
+        this(parent, child, childName, 0);
     }
 
-    public ParentChild(P parent, C child, int level) {
+    public ParentChild(P parent, C child, String childName, int level) {
+        this.childName = childName;
         this.parent = parent;
         this.child = child;
         this.level = level;
     }
 
-    public P parent(){
+    public P parent() {
         return parent;
     }
 
-    public C child(){
+    public C child() {
         return child;
+    }
+
+    public int level() {
+        return level;
+    }
+
+    public String childName() {
+        return childName;
     }
 }
