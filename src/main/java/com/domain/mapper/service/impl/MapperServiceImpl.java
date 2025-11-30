a397fb02d13633b47b97b125830211c4d1cb3c4c
sougata <sougata19582001@gmail.com>
2025-11-30 14:34:08 +0530

    added to entity method

--- src/main/java/com/domain/mapper/service/impl/MapperServiceImpl.java
+++ src/main/java/com/domain/mapper/service/impl/MapperServiceImpl.java
@@ -34,7 +34,97 @@ public class MapperServiceImpl implements MapperService {
 
     @Override
     public MasterEntity toEntity(MasterDto dto) {
-        return null;
+        try {
+            if (dto == null) return null;
+            MasterEntity res = null;
+            Queue<ParentChild<MasterEntity, MasterDto>> queue = new LinkedList<>();
+            Set<Edge> visited = new HashSet<>();
+
+            queue.add(new ParentChild<>(null, dto, "super"));
+            visited.add(new Edge(null, dto, "super"));
+
+            while (!queue.isEmpty()) {
+                ParentChild<MasterEntity, MasterDto> u = queue.poll();
+
+                MasterEntity parent = u.parent();
+                MasterDto child = u.child();
+                String relationName = u.relationName();
+                Class<? extends MasterEntity> entityClass = map.getDtoEntityMap().get(child.getClass());
+                if (entityClass == null) {
+                    throw new IllegalStateException("No ENTITY Mapping found for %s".formatted(child.getClass()));
+                }
+
+                MasterEntity entity = entityClass.getDeclaredConstructor().newInstance();
+
+                for (Field ef : entity.getClass().getDeclaredFields()) {
+                    ef.setAccessible(true);
+                    Field df = getDeclaredField(child.getClass(), ef.getName());
+                    df.setAccessible(true);
+
+                    Object value = df.get(child);
+
+                    if (Collection.class.isAssignableFrom(ef.getType())) {
+                        if (value != null) {
+                            Collection<?> collection = (Collection<?>) value;
+                            if (collection.isEmpty()) {
+                                ef.set(entity, new HashSet<>());
+                            } else {
+                                for (Object o : collection) {
+                                    Edge edge = new Edge(child, o, df.getName());
+                                    if (!visited.contains(edge)) {
+                                        visited.add(edge);
+                                        queue.add(new ParentChild<>(entity, (MasterDto) o, ef.getName()));
+                                    }
+                                }
+                            }
+                        }
+                    } else if (isComplex(ef)) {
+                        if (value != null) {
+                            Edge edge = new Edge(child, value, df.getName());
+                            if (!visited.contains(edge)) {
+                                visited.add(edge);
+                                queue.add(new ParentChild<>(entity, (MasterDto) value, ef.getName()));
+                            }
+                        }
+                    } else {
+                        if (value != null) {
+                            ef.set(entity, value);
+                        }
+                    }
+                }
+
+                if (parent == null) {
+                    res = entity;
+                } else {
+                    for (Field pf : parent.getClass().getDeclaredFields()) {
+                        pf.setAccessible(true);
+                        if (Collection.class.isAssignableFrom(pf.getType())) {
+                            Type genericType = pf.getGenericType();
+                            if (genericType instanceof ParameterizedType) {
+                                Type actualType = ((ParameterizedType) genericType).getActualTypeArguments()[0];
+                                if (actualType == entity.getClass() && pf.getName().equals(relationName)) {
+                                    Collection<MasterEntity> collection = (Collection<MasterEntity>) pf.get(parent);
+                                    if (collection == null) {
+                                        collection = new HashSet<>();
+                                        collection.add(entity);
+                                        pf.set(parent, collection);
+                                    } else {
+                                        collection.add(entity);
+                                    }
+                                }
+                            }
+                        } else if (isComplex(pf)) {
+                            if (pf.getType() == entity.getClass() && pf.getName().equals(relationName)) {
+                                pf.set(parent, entity);
+                            }
+                        }
+                    }
+                }
+            }
+            return res;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -57,7 +147,7 @@ public class MapperServiceImpl implements MapperService {
                 int level = u.level();
                 MasterDto parent = u.parent();
                 MasterEntity child = u.child();
-                String childName = u.childName();
+                String relationName = u.relationName();
                 Class<? extends MasterDto> dtoClass;
 
                 if (child instanceof HibernateProxy) {
@@ -130,18 +220,19 @@ public class MapperServiceImpl implements MapperService {
                             Type genericType = pf.getGenericType();
                             if (genericType instanceof ParameterizedType) {
                                 Type actualType = ((ParameterizedType) genericType).getActualTypeArguments()[0];
-                                if (actualType == dto.getClass() && pf.getName().equals(childName)) {
+                                if (actualType == dto.getClass() && pf.getName().equals(relationName)) {
                                     Collection<MasterDto> collection = (Collection<MasterDto>) pf.get(parent);
                                     if (collection == null) {
                                         collection = new HashSet<>();
                                         collection.add(dto);
                                         pf.set(parent, collection);
+                                    } else {
+                                        collection.add(dto);
                                     }
-                                    collection.add(dto);
                                 }
                             }
                         } else if (isComplex(pf)) {
-                            if (pf.getType() == dto.getClass() && pf.getName().equals(childName)) {
+                            if (pf.getType() == dto.getClass() && pf.getName().equals(relationName)) {
                                 pf.set(parent, dto);
                             }
                         }
